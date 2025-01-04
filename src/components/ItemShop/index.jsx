ff9cabfed884260aabf2f7dba65bf71121bd8d57
Calixto <fcalixto318@gmail.com>
2025-01-04 02:28:13 -0300

    produtos

--- src/components/ItemShop/index.jsx
+++ src/components/ItemShop/index.jsx
@@ -10,12 +10,16 @@ const ItemShop = ({ img, nomeProduto, valorProduto }) => {
   return (
     <div className="item-shop">
       <img src={img} alt={nomeProduto} className="item-shop-img" />
-      <div>
+      <div className="item-name">
         <h3>{nomeProduto}</h3>
-        <FaHeart style={{ color: isFavorited ? "red" : "gray" }}  onClick={toggleFavorite}/>
+        <FaHeart className="
+        icon-heart" style={{ color: isFavorited ? "red" : "gray" }}  onClick={toggleFavorite}/>
       </div>
       
       <p>{valorProduto}</p>
+      <div className="add-carinho">
+        <a href="" className="btn-carinho"> Adicionar ao carinho</a>
+      </div>
     </div>
   );
 };
